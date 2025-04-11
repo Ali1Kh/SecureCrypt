51d246a69b477e95c4ae0548b8242d093b40bbbf
Abdoo14785 <abdalla01145854052@gmail.com>
2025-04-11 16:43:09 +0200

    Add Flutter

---
+++ android/app/src/main/kotlin/com/example/secure_crypt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.secure_crypt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyAppp());
+}
+class MyAppp extends StatelessWidget {
+  const MyAppp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

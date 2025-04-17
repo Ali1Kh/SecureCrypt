bb5d3be234deda0a0e49ffc4eded732d83799e56
Ali Khaled <alielsaadany94@gmail.com>
2025-04-17 20:02:04 +0200

    Demiana Tasks

--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,7 @@ function copyToClipboard(id) {
   let text = document.getElementById(id).value;
   navigator.clipboard.writeText(text);
 }
+
+function navigateTo(page) {
+  window.location.href = page;
+}

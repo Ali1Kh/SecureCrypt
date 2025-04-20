eaf81d31ce176a1a0a92192140a0f39ee3dce940
Ali Khaled <alielsaadany94@gmail.com>
2025-04-20 22:13:35 +0200

    Typed animation

--- JS/index.js
+++ JS/index.js
@@ -6,3 +6,23 @@ function copyToClipboard(id) {
 function navigateTo(page) {
   window.location.href = page;
 }
+
+new Typed("#typedElement", {
+  strings: [
+    "New Cairo Technological University", 
+    "f8b2aa1161b59c8c65c050173efc2e9fad23e143", 
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
+
+new Typed("#typedElementStatus", {
+  strings: ["Processing...", "Encrypting..."], 
+  typeSpeed: 0,
+  backSpeed: 0,
+  backDelay: 5700,
+  loop: true,
+  showCursor: false,
+});
+ 
